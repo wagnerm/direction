136541b01f8aa0729af46c9582f3d6431778550f
Max Wagner <wagnerm@github.com>
2020-09-18 13:43:11 -0600

    Fetch point

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod api;
+mod point;
 
 fn main() {
     let weather = api::Weather::new().expect("Failed to initialize weather reading!");
-
-    weather.fetch_api().unwrap();
+    let p = point::Point::get(weather).expect("Failed to find point!");
+    println!("Point is {:?}", p.properties.forecast);
 }
