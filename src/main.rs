1953d46ea2b635effa7311cab05b9e063f7063e6
Max Wagner <wagnerm@github.com>
2020-09-22 14:56:38 -0600

    Use cooridnate of WeatherClient

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod client;
 mod point;
 
 fn main() {
-    let weather = client::WeatherClient::new(String::from("40.5853,-105.0844"))
+    let weather = client::WeatherClient::new("40.5853,-105.0844")
         .expect("Failed to initialize weather reading!");
     let f = weather.get_forecast().expect("Cound not get forecast!");
     for p in f.properties.periods {
--- src/point.rs
+++ src/point.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-use super::client::WeatherClient;
-
 #[derive(Deserialize, Serialize)]
 pub struct Point {
     pub id: String,
