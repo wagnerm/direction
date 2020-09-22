dabfd1e199d71440ca5969eae0e642e00ee292f3
Max Wagner <wagnerm@github.com>
2020-09-22 14:43:35 -0600

    Moving some the request bits to the client
    This seems less fragmented than before.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ mod client;
 mod point;
 
 fn main() {
-    let weather = client::WeatherClient::new().expect("Failed to initialize weather reading!");
-    let p = point::Point::fetch(&weather).expect("Failed to find point!");
-    let f = p.get_forecast(&weather).expect("Cound not get forecast!");
+    let weather = client::WeatherClient::new(String::from(***REMOVED***))
+        .expect("Failed to initialize weather reading!");
+    let p = weather.get_point().expect("Count not get point!");
+    let f = weather.get_forecast(&p).expect("Cound not get forecast!");
     for p in f.properties.periods {
         println!("{} {} {}", p.name, p.windSpeed, p.windDirection);
     }
