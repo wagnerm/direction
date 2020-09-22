ad893b6122012001321562f73ee3caad23a70f89
Max Wagner <wagnerm@github.com>
2020-09-22 14:12:25 -0600

    Fetch grid and all available periods
    Print them out.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod point;
 
 fn main() {
     let weather = api::Weather::new().expect("Failed to initialize weather reading!");
-    let p = point::Point::get(weather).expect("Failed to find point!");
-    println!("Point is {:?}", p.properties.forecast);
+    let p = point::Point::fetch(&weather).expect("Failed to find point!");
+    let f = p.get_forecast(&weather).expect("Cound not get forecast!");
+    for p in f.properties.periods {
+        println!("{} {} {}", p.name, p.windSpeed, p.windDirection);
+    }
 }
