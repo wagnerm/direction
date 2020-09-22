f0e585298da1923e0dd8cd4bf17057d2524eb265
Max Wagner <wagnerm@github.com>
2020-09-22 14:12:25 -0600

    Fetch grid and all available periods
    Print them out.

--- src/point.rs
+++ src/point.rs
@@ -13,11 +13,39 @@ pub struct Property {
     pub forecast: String,
 }
 
-impl Point {
-    pub fn get(w: Weather) -> Result<Point, reqwest::Error> {
-        let url = String::from("https://api.weather.gov/points/40.5853,-105.0844");
-        let json: Point = w.get(&url)?.json()?;
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Forecast {
+    pub properties: ForecastProperty,
+}
 
-        Ok(json)
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ForecastProperty {
+    pub periods: Vec<Period>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Period {
+    pub number: usize,
+    pub name: String,
+    pub startTime: String,
+    pub endTime: String,
+    pub windSpeed: String,
+    pub windDirection: String,
+    pub detailedForecast: String,
+}
+
+impl Point {
+    pub fn fetch(w: &Weather) -> Result<Point, reqwest::Error> {
+        let url = String::from("https://api.weather.gov/points/40.5853,-105.0844");
+        let point: Point = w.get(&url)?.json()?;
+
+        Ok(point)
+    }
+
+    pub fn get_forecast(&self, w: &Weather) -> Result<Forecast, reqwest::Error> {
+        let url = String::from(self.properties.forecast.clone());
+        let forecast: Forecast = w.get(&url)?.json()?;
+
+        Ok(forecast)
     }
 }
