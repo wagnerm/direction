69a95112811cc174c1780a5ca93ce32168120a17
Max Wagner <wagnerm@github.com>
2020-09-22 21:31:09 -0600

    Init dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+
+mod chat;
 mod client;
 mod point;
 
 fn main() {
+    dotenv().ok();
+
     let weather = client::WeatherClient::new("40.5853,-105.0844")
         .expect("Failed to initialize weather reading!");
     let f = weather.get_forecast().expect("Cound not get forecast!");
